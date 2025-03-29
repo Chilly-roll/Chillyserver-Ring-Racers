2599326fcf0400956208b1fc9cccbe4457ec1c74
Chilly-roll <31173350+Chilly-roll@users.noreply.github.com>
2025-03-29 12:48:16 -0500

    Initial Commit

---
+++ DL_ChillyServer_ColorRepack.lua
@@ -0,0 +1,81 @@
+freeslot(
+"SKINCOLOR_NEPTUNO",
+"SKINCOLOR_KRITZKRIEG",
+"SKINCOLOR_VACCINATOR",
+"SKINCOLOR_SLUGGER",
+"SKINCOLOR_DAVE_SKAIAN",
+"SKINCOLOR_GAMZEE_SKAIAN",
+"SKINCOLOR_FEVER",
+"SKINCOLOR_MAIZE"
+)
+
+skincolors[SKINCOLOR_NEPTUNO] = {
+    name = "Neptuno",
+    ramp = {146,147,148,164,165,165,186,168,167,168,168,187,169,253,253,253
+},
+    invcolor = SKINCOLOR_CREAMSICLE,
+    invshade = 17,
+    chatcolor = V_PURPLEMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_KRITZKRIEG] = {
+    name = "Kritzkrieg",
+    ramp = {0,252,252,200,194,40,43,71,47,197,198,198,199,199,199,199
+},
+    invcolor = SKINCOLOR_CREAMSICLE,
+    invshade = 23,
+    chatcolor = V_REDMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_VACCINATOR] = {
+    name = "Vaccinator",
+    ramp = {0,144,144,145,148,134,135,137,138,137,138,138,139,139,139,139
+},
+    invcolor = SKINCOLOR_CREAMSICLE,
+    invshade = 24,
+    chatcolor = V_BLUEMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_SLUGGER] = {
+    name = "Slugger",
+    ramp = {0,208,49,50,50,227,227,230,232,232,197,197,199,199,199,199
+},
+    invcolor = SKINCOLOR_BLACK,
+    invshade = 25,
+    chatcolor = V_YELLOWMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_DAVE_SKAIAN] = {
+    name = "Time",
+    ramp = {34, 35, 35, 40, 40, 41, 41, 44, 44, 45, 45, 46, 46, 47, 47, 30
+    },
+    invcolor = SKINCOLOR_SUNSLAM,
+    invshade = 11, 
+    chatcolor = V_REDMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_GAMZEE_SKAIAN] = {
+    name = "Capricorn",
+    ramp = {73, 53, 35, 2, 161, 162, 163, 164, 165, 166, 167, 168, 168, 169, 254, 31
+    },
+    invcolor = SKINCOLOR_TURTLE,
+    invshade = 14, 
+    chatcolor = V_PURPLEMAP,
+    accessible = true
+}
+skincolors[SKINCOLOR_FEVER] = {
+	name = "Fever",
+	ramp = {80,82,83,72,64,65,222,33,203,204,206,207,43,45,46,47},
+	invcolor = SKINCOLOR_CERULEAN,
+	invshade = 7, 
+	chatcolor = V_PINKMAP,
+	accessible = true
+}
+skincolors[SKINCOLOR_MAIZE] = {
+    name = "Maize",
+    ramp = {82,73,74,75,66,66,67,68,105,106,107,108,109,110,111,31},
+    invcolor = SKINCOLOR_HANDHELD,
+    invshade = 7,
+    chatcolor = V_YELLOWMAP,
+    accessible = true
+    }
